f4bd3c270ff6ca3c28e4272e328e2780e709bf4a
Pratik Giri <pratikgiriraj2002@gmail.com>
2025-09-07 21:48:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed =new typed(".text",{
+    strings:["Fronted Devloper","Web Developer","Graphic Designer","Java Full Stack","Python Full Stack"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
